e309fc71115a08f0cc136b1f0e1769fe3321d51a
Kunal Kankariya <kkankariya007@gmail.com>
2023-05-05 09:47:17 +0530

    App Pending

---
+++ SyntaxChecker.py
@@ -0,0 +1,169 @@
+# Token class
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __repr__(self):
+        return 'Token({type}, {value})'.format(
+            type=self.type,
+            value=repr(self.value)
+        )
+
+# Token types
+INTEGER = 'INTEGER'
+STRING = 'STRING'
+PLUS = 'PLUS'
+MINUS = 'MINUS'
+MULTIPLY = 'MULTIPLY'
+DIVIDE = 'DIVIDE'
+POWER = 'POWER'
+EOF = 'EOF'
+
+
+# Lexical Analyzer
+class Lexer:
+    def __init__(self, source_code):
+        self.source_code = source_code
+        self.pos = 0
+        self.current_char = self.source_code[self.pos]
+
+    def advance(self):
+        self.pos += 1
+        if self.pos < len(self.source_code):
+            self.current_char = self.source_code[self.pos]
+        else:
+            self.current_char = None
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def get_next_token(self):
+        while self.current_char is not None:
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+            if self.current_char.isdigit():
+                return self.integer()
+            if self.current_char == '"':
+                return self.string()
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+            if self.current_char == '*':
+                self.advance()
+                return Token(MULTIPLY, '*')
+            if self.current_char == '/':
+                self.advance()
+                return Token(DIVIDE, '/')
+            if self.current_char == '^':
+                self.advance()
+                return Token(POWER, '^')
+            
+            raise SyntaxError("Invalid token")
+
+        return Token(EOF, None)
+
+    def integer(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return Token(INTEGER, int(result))
+
+    def string(self):
+        self.advance()
+        result = ''
+        while self.current_char is not None and self.current_char != '"':
+            result += self.current_char
+            self.advance()
+        if self.current_char == '"':
+            self.advance()
+            return Token(STRING, result)
+        else:
+            raise SyntaxError("Invalid string")
+
+# Syntax Analyzer
+class Parser:
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = self.lexer.get_next_token()
+
+    def error(self):
+        raise SyntaxError("Invalid syntax")
+
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error()
+
+    def factor(self):
+        token = self.current_token
+        if token.type == INTEGER:
+            self.eat(INTEGER)
+            return token.value
+        elif token.type == STRING:
+            self.eat(STRING)
+            return token.value
+        else:
+            self.error()
+
+    def term(self):
+        result = self.factor()
+
+        while self.current_token.type in (MULTIPLY, DIVIDE):
+            token = self.current_token
+            if token.type == MULTIPLY:
+                self.eat(MULTIPLY)
+                result *= self.factor()
+            elif token.type == DIVIDE:
+                self.eat(DIVIDE)
+                result /= self.factor()
+
+        return result
+        
+    def expr(self):
+        result = self.term()
+
+        while self.current_token.type in (PLUS, MINUS):
+            token = self.current_token
+            if token.type == PLUS:
+                self.eat(PLUS)
+                result += self.term()
+            elif token.type == MINUS:
+                self.eat(MINUS)
+                result -= self.term()
+
+        return result
+
+class SyntaxChecker:
+    def __init__(self, source_code):
+        self.lexer = Lexer(source_code)
+        self.parser = Parser(self.lexer)
+
+    def check_syntax(self):
+        try:
+            self.parser.expr()
+            print("Syntax is correct")
+        except SyntaxError as e:
+            print("Syntax error: ", e)
+
+
+exp= input("Enter the expression:")
+SyntaxChecker(exp).check_syntax()
+
+
+
+
+
+
+
+
+
+
+
